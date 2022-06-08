4b7ef65ff0cd7514d320cf27c9476dd5e2f170f6
tdhood <taylordannhood@gmail.com>
2022-06-08 13:37:57 -0700

    add function

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
 # Put your app in here.
+from flask import Flask, request
+from operations import add, sub, mult, div
+app = Flask(__name__)
+
+@app.get('/add')
+def add_app():
+    a = int(request.args['a'])
+    b = int(request.args['b'])
+    sum = add(a,b)
+    return f"Sum of {a} and {b} is {sum}"
